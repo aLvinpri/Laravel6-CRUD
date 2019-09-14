40c06023bafb66cc9e9a4f503e9ef509cb69e15e
aLvinpri <alvhient@gmail.com>
2019-09-14 22:22:03 +0700

    vid 18 - 18:15

--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,25 @@
 |
 */
 
+// Cara default dari laravel dalam mengakses view
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
-Route::get('/about', function () {
-    $nama = 'ALFIN PRIANDI';
-    return view('about', ['nama' => $nama]);
-});
+// Route::get('/about', function () {
+//     $nama = 'ALFIN PRIANDI';
+//     return view('about', ['nama' => $nama]);
+// });
 
-Route::get('/mahasiswa', function () {
-    return view('mahasiswa');
-});
+// Route::get('/mahasiswa', function () {
+//     return view('mahasiswa');
+// });
+
+// Cara menjadikan route mengakses controller untuk mengakses view atau model
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/mahasiswa', 'PagesController@mahasiswa');
