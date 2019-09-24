19e1b77f3ff67260177ea48b83c24975958aa13a
aLvinpri <alvhient@gmail.com>
2019-09-24 20:59:18 +0700

    add something note

--- MyNote.php
+++ MyNote.php
@@ -1,5 +1,34 @@
 <?php
 
+Cara menginstall laravel
+1. buka terminal di htdocs kemudian ketik :
+    => composer create-project --prefer-dist laravel/laravel (namaFolder)
+    contoh : composer create-project --prefer-dist laravel/laravel myProjectLaravel
+2. buka terminal di folder yang akkan dijadikan project, kemudian buka terminal dan ketik :
+    => composer global require laravel/installer
+3. menginstall laravel versi sebelumnya atau versi lama
+    => composer create-project --prefer-dist laravel/laravel blog "5.8.*"
+        atau
+    => composer create-project --prefer-dist laravel/laravel blog "5.6.*"
+        atau
+    => composer create-project --prefer-dist laravel/laravel blog "5.3.*"
+        dst
+    *latest version of laravel (
+                                5.8, 
+                                5.7, 
+                                5.6,
+                                5.5,
+                                5.4,
+                                5.3,
+                                5.2,
+                                5.1,
+                                5.0,
+                                4.2
+                                ).
+4. Cara mendowload laravel 5.0, dan 4.2
+    => composer global require "laravel/installer=~1.1"
+    => composer create-project laravel/laravel {directory} 4.2 --prefer-dist
+
 Cara membuat Controller menggunakan Terminal/Command Prompt
 Ketik : php artisan make:controller NamaController Parameter
 Contoh :  1. php artisan make:controller PagesController
